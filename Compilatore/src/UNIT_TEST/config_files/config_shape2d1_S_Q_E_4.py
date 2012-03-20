50569fdd8b1113d13341a61462982d1b53df4ad0
andre.lotta86 <andre.lotta86@62475b15-cea0-10ed-fae3-35a356ea42c1>
2012-03-20 13:52:15 +0000

    path sistemati, openMP non funziona

--- Compilatore/src/UNIT_TEST/config_files/config_shape2d1_S_Q_E_4.py
+++ Compilatore/src/UNIT_TEST/config_files/config_shape2d1_S_Q_E_4.py
@@ -9,23 +9,23 @@ QT                  = True
 DOMAIN_EDGE_SIZE    = 32
 ITERATIONS          = 2
 PARALLELISM_DEGREE  = 4
-OUTPUT_FILENAME     = "../testBench/compilato.c"
+OUTPUT_FILENAME     = "./testBench/compilato.c"
 STEP_MODEL_OUTPUT   = "./stepModel"
 MAGIC_PARAMETER     = 7
 MAGIC_THRESHOLD_FOR_OFFSET  = 4
 
 #questo e il path al file dalla shape
-SHAPE_FILENAME      = "../shapes/shape2d1"
+SHAPE_FILENAME      = "./shapes/shape2d1"
 
 # questo file contiene la #define di MACRO
 HEADER_FUNZIONE     = "../headers/macro2d1.h"
 
 
 #questa cartella contiene il supporto a runtime del compilatore e i dati per la computazione
-TESTBENCH_DIR       = "../testBench/"
+TESTBENCH_DIR       = "./testBench/"
 
 #in questa cartella sono salvati diversi chunk di header C che il compilatore andra a inserire nel file compilato
-HEADERS_DIR         = "../headers/"
+HEADERS_DIR         = "./headers/"
 
 # questo file contiene la matrice risulato per la computazione specificata in questo config
 RISULTATO           = "../UNIT_TEST/matrices/2d1_32"
